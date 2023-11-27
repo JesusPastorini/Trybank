463e3a4d00ee831aa0124766bd6261889da8a5d5
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-11-26 23:07:21 -0300

    req 4 e 5

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -109,13 +109,21 @@ public class TrybankLib
     // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        return Bank[loggedUser, 3];
     }
 
     // 5. Construa a funcionalidade de depositar dinheiro
     public void Deposit(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        Bank[loggedUser, 3] += value;
     }
 
     // 6. Construa a funcionalidade de sacar dinheiro
